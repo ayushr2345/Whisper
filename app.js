dc6cdce13fc1e4e59fbd77f162f12210457c4725
ayushr2345 <ayush.r2345@gmail.com>
2022-01-24 19:11:28 +0530

    Implement hashing and salting rounds (bcrypt)

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-require('dotenv').config();
+// Hash md5 is not so good for passwords so we use salting
+// password + random salt convert to hash store salt and hash in db
+// more secure, repeat the step a number of times - bcrypt
+
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const port = 3000;
-const md5 = require('md5');
 
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 mongoose.connect('mongodb://localhost:27017/userDB');
+const saltRounds = 10;
 
 const userSchema = new mongoose.Schema({ 
     email: String,
@@ -32,32 +36,39 @@ app.get('/register', (req, res) => {
 });
 
 app.post('/register', (req, res) => {
-    const newUser = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
-    newUser.save((err) => {
-        if (err) {
-            console.log(err);
-        } else {
-            res.render('secrets')
-        }
+    bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        });
+        newUser.save((err) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render('secrets')
+            }
+        }); 
     });
 });
 
 app.post('/login', (req, res) => {
     const email = req.body.username;
-    const password = req.body.password;
 
     User.findOne({email: email}, (err, foundUser) => {
         if (err) {
             console.log(err);
         } else {
-            if (foundUser && foundUser.password === md5(password)) {
-                res.render('secrets');
-            } else {
-                console.log("Not found");
-            }
+            bcrypt.compare(req.body.password, foundUser.password, (err, result) => {
+                if (err) {
+                    console.log(err); 
+                } else {
+                    if (result && foundUser) {
+                        res.render('secrets');
+                    } else {
+                        console.log("Not found");
+                    }
+                }
+            });
         }
     });
 });
