edf2ac0ed31d6546cb904a3c23e6ffef06128bef
ayushr2345 <ayush.r2345@gmail.com>
2022-01-24 18:31:01 +0530

    Level 1 and 2

--- app.js
+++ app.js
@@ -1,110 +1,79 @@
-//jshint esversion:6
 require('dotenv').config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const ejs = require("ejs");
-const mongoose = require("mongoose");
-const session = require("express-session");
-const passport = require("passport");
-const passportLocalMongoose = require("passport-local-mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const ejs = require('ejs');
+const bodyParser = require('body-parser');
+const encrypt = require('mongoose-encryption');
+const port = 3000;
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+mongoose.connect('mongodb://localhost:27017/userDB');
 
-app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.use(session({
-    secret: "Our Little Secret.",
-    resave: false,
-    saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
-
-mongoose.set("useCreateIndex", true);
-
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema({ 
     email: String,
     password: String
 });
 
-userSchema.plugin(passportLocalMongoose);
+// Level 1 was plain string
 
-const User = new mongoose.model("User", userSchema);
+// Level 2- mongoose-encryption (encrypts the database with some key)
+/*
+const secret = "Thisissecret";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ['password']});
+*/
 
-passport.use(User.createStrategy());
+// But the secret key can be seen by anyone
+// hence we use dotenv to hide it in other file
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser);
+const User = mongoose.model('User', userSchema);
 
-
-app.get('/', function(req, res){
-    res.render("home");
+app.get('/', (req, res) => {
+    res.render('home');
 });
 
-app.get('/login', function(req, res){
-    res.render("login");
+app.get('/login', (req, res) => {
+    res.render('login');
 });
 
-app.get('/register', function(req, res){
-    res.render("register");
+app.get('/register', (req, res) => {
+    res.render('register');
 });
 
-app.get('/secrets', function(req, res) {
-    if(req.isAuthenticated()){
-        res.render('/secrets');
-    } else{
-        res.render('/login');
-    }
-});
-
-app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-});
-
-app.post('/register', function(req, res){
-    User.register({username: req.body.username}, req.body.password, function(err, user){
-        if(err){
-            console.log(err);
-            res.redirect('/register');
-        }else{
-            passport.authenticate("local")(req, res, function(){
-                res.redirect('/secrets');
-            })
-        }
-    })
-
- 
-});
-
-app.post('/login', function(req, res){
-    const user = new User({
-        username: req.body.username,
+app.post('/register', (req, res) => {
+    const newUser = new User({
+        email: req.body.username,
         password: req.body.password
     });
-
-    req.login(user, function(err) {
-        if(err){
+    newUser.save((err) => {
+        if (err) {
             console.log(err);
-        }else{
-            passport.authenticate("local")(req, res, function() {
-                res.redirect('/secrets');
-            })
+        } else {
+            res.render('secrets')
         }
-        
-    })
-
+    });
 });
 
+app.post('/login', (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
 
-app.listen(3002, function(){
-    console.log("Server running on port 3001");
-})
+    User.findOne({email: email}, (err, foundUser) => {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser && foundUser.password === password) {
+                res.render('secrets');
+            } else {
+                console.log("Not found");
+            }
+        }
+    });
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+});
